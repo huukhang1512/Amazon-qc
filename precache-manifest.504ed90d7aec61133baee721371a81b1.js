a0b466a87cdf2d839a9adb44a27ee195df233d2e
Hữu Khang <huukhang1512@gmail.com>
2020-08-24 16:56:39 +1000

    Updates

---
+++ precache-manifest.504ed90d7aec61133baee721371a81b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131b9d283f7d50fd95904582cc8db3ad",
+    "url": "/amazon-qc/index.html"
+  },
+  {
+    "revision": "7585f45f3dae7acbd6ce",
+    "url": "/amazon-qc/static/css/main.c8cbea65.chunk.css"
+  },
+  {
+    "revision": "f34e0d4206e98bc8a081",
+    "url": "/amazon-qc/static/js/2.07a160e5.chunk.js"
+  },
+  {
+    "revision": "df3d5664865bc0d56b8c09e81f287052",
+    "url": "/amazon-qc/static/js/2.07a160e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7585f45f3dae7acbd6ce",
+    "url": "/amazon-qc/static/js/main.33be454b.chunk.js"
+  },
+  {
+    "revision": "2a38c21fb60a5b756982",
+    "url": "/amazon-qc/static/js/runtime-main.11973ef6.js"
+  }
+]);
