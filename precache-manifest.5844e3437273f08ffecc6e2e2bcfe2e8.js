245fc743c19b7dbd4854c40544f0c33537bbe60f
Hữu Khang <huukhang1512@gmail.com>
2020-09-10 23:30:39 +1000

    Updates

---
+++ precache-manifest.5844e3437273f08ffecc6e2e2bcfe2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7aad2fd9a7c3813954f5187d3bea5e",
+    "url": "/Amazon-qc/index.html"
+  },
+  {
+    "revision": "3b92e5aa8ce014689f3a",
+    "url": "/Amazon-qc/static/css/main.c8cbea65.chunk.css"
+  },
+  {
+    "revision": "ae142190a5f2245fa118",
+    "url": "/Amazon-qc/static/js/2.e81759df.chunk.js"
+  },
+  {
+    "revision": "ab78e7762b21461e5bfe12c7a276b4ce",
+    "url": "/Amazon-qc/static/js/2.e81759df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b92e5aa8ce014689f3a",
+    "url": "/Amazon-qc/static/js/main.a47d2fdb.chunk.js"
+  },
+  {
+    "revision": "a14024234c0fab191262",
+    "url": "/Amazon-qc/static/js/runtime-main.75c2b791.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Amazon-qc/precache-manifest.ada010b67d1078a5eabbe617552d0f64.js"
+  "/Amazon-qc/precache-manifest.5844e3437273f08ffecc6e2e2bcfe2e8.js"
 );
 
 self.addEventListener('message', (event) => {
